dfce313a4eb315733d309336ef6825b5dd07c51c
ahb-0 <admin@freifunknord.de>
2017-11-24 19:13:44 +0100

    install.sh angepasst

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# GO API download
 cd /usr/local/
 wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
 tar xvf go1.8.linux-amd64.tar.gz
@@ -9,19 +10,28 @@ export GOPATH=/opt/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 EOF
 
+## Yanic Installation
 go get -v -u github.com/FreifunkBremen/yanic
+
+### Yanic cfg setzen
 cd /etc
-wget
+wget https://github.com/Freifunk-AHB/nord-ahb-yanic-cfg/raw/master/etc/yanic.conf
 
 mkdir -p /var/lib/yanic
 touch /var/log/yanic.log
 useradd yanic
+
+### Ausgabe Verzeichnise erstellen
 mkdir -p /var/www/html/meshviewer/data/
 chown yanic /var/log/yanic.log /var/lib/yanic /var/www/html/meshviewer /var/www/html/meshviewer/data
 cp /opt/go/src/github.com/FreifunkBremen/yanic/contrib/init/linux-systemd/yanic.service /lib/systemd/system/
+
+### systemd Dienst aktivieren
 systemctl daemon-reload
 systemctl enable yanic
 systemctl start yanic
+
+### iptables Regeln setzen
 touch /etc/iptables.d/500-Allow-respondd
 cat <<-EOF>> /etc/iptables.d/500-Allow-respondd
 # Allow Service respondd
@@ -32,4 +42,3 @@ ip46tables -A mesh-input -p udp -m udp --dport 45123    -j ACCEPT
 EOF
 
 build-firewall
-
